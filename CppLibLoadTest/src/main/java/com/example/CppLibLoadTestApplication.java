70c16d41267c3c8cba98e4294ed37a3bbbf53045
antropoff <antropoff@gmail.com>
2022-12-06 09:17:38 +0700

    Dummy proj commit

---
+++ CppLibLoadTest/src/main/java/com/example/CppLibLoadTestApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CppLibLoadTestApplication {
+    public static void main(String[] args) {
+
+        System.out.print("ASDF");
+    }
+}
